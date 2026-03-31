b96cccb4c17f18ae72d324291eec96c8d979ef7c
Chenyun(Rachel)Ruan <rachelruancy@gmail.com>
2026-03-31 16:51:28 -0700

    Add files via upload

---
+++ connect4_ui_ai.py
@@ -0,0 +1,24 @@
+# connect4_ui_ai.py
+#
+# ICS 32 
+# Project #2
+#
+# This module contains utility functions to implement the AI player 
+# for the console UI Connect Four game.
+# 
+
+import connect4
+import random
+
+DROP = 1
+POP = 2
+
+# New function
+def ai_move(state: connect4.GameState) -> tuple[int, int]:
+    '''
+    Asks the AI to choose a move, returning a tuple whose first element
+    is DROP or POP and whose second element is a valid column number.
+    Hint: helper functions would use useful here.
+    '''
+
+    pass 
